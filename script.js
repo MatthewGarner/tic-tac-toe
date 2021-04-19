c8abe4257d0add2bd02a687f7306df30f55eaf8e
Matthew Garner <1matthewgarner@gmail.com>
2021-04-19 18:41:31 +0100

    add team selection

--- script.js
+++ script.js
@@ -1,3 +1,47 @@
+//Global variables
+
+const teamButtons = document.querySelectorAll('.team-select');
+const noughts = 'O';
+const crosses = 'X';
+
+//Create players
+
+const playerFactory = (name, team) => {
+    let teamIcon;
+    const getName = () => name;
+    const setName = (nameInput) => {
+        name = nameInput;
+    }
+    const getTeamName = () => team;
+    const setTeam = (selectedTeam) => {
+        team = selectedTeam;
+        _setTeamIcon(team);
+    };
+
+    const _setTeamIcon = (team) => {
+        switch(team) {
+            case 'noughts':
+                teamIcon = noughts;
+                break;
+            case 'crosses':
+                teamIcon = crosses;
+                break;
+            default: 
+                console.log('Error: invalid team selected');
+        }
+    }
+    const getTeamIcon = () => teamIcon
+
+
+    return {
+        getName,
+        setName,
+        getTeamName, 
+        setTeam,
+        getTeamIcon
+    };
+};
+
 //Gameboard module - store information and functions related to the game board
 
 const Gameboard = (() => {
@@ -11,31 +55,43 @@ const Gameboard = (() => {
 
             square.addEventListener('click', e => {
                 const pressedSquare = e.target.dataset.squareNumber;
-                _setSquare(pressedSquare);
+                
+                _setSquare(pressedSquare, game.player1.getTeamIcon());
             });
-
-            (i % 2 == 0) ? 
-            square.querySelector('p').textContent = 'X' :
-            square.querySelector('p').textContent = 'O';  
-            
-        })
-    }
+        });
+    };
 
     const _setSquare = (squareNumber, team) => {
         const locationInArray = squareNumber - 1;
+        gameBoard[locationInArray] = team;
         
-        const newValue = team || 'X';
 
-        gameBoard[locationInArray] = newValue;
 
         displayController.renderBoard();
-    }
+    };
 
     const getBoardSquare = (number) => gameBoard[number];
+
+    const getUnoccupiedSquares = () => {
+        let availableSquares = [];
+
+        console.table(gameBoard);
+
+        gameBoard.forEach((square, i) => {
+            if ((square == 'X' || square == 'O')) {
+                availableSquares.push(i);
+            }
+            
+            
+
+        })
+        return availableSquares;
+    }
     
     return {
         getBoardSquare, 
-        initialiseGameBoard
+        initialiseGameBoard, 
+        getUnoccupiedSquares
     };
 })();
 
@@ -45,39 +101,87 @@ const displayController = (() => {
 
     const renderBoard = () => {
         const boardSquares = document.querySelectorAll('.board-square');
-        boardSquares.forEach((square, i) => {
-            
+        boardSquares.forEach((square, i) => {  
             const latestSquareValue = Gameboard.getBoardSquare(i);
             square.querySelector('p').textContent = latestSquareValue;
-
     });
 }
 
-    return {
-        renderBoard
-    };
-})();
-
-//Create players
-
-const playerFactory = (name, team) => {
-
-    const getName = () => name;
-    const getTeam = () => team;
-    const setTeam = (selectedTeam) => {
-        team = selectedTeam;
+    const showPlayerNames = () => {
+        const playerArea = document.getElementById('match-title');
+        playerArea.textContent = `${game.player1.getName()} vs ${game.computer.getName()}`;
     }
 
 
     return {
-        getName, 
-        getTeam, 
-        setTeam
+        renderBoard, 
+        showPlayerNames
     };
-};
+})();
+
+//Game module to control flow
+
+const game = (() => {
+
+    const computer = playerFactory("Computer");
+    const player1 = playerFactory();
+
+    const getPlayerNameFromUser = (message) => {
+        let playerName;
+    
+        while(true) {
+            playerName = prompt(message);
+            if(playerName !== undefined && playerName != "") {
+                break;
+            }
+        }
+        return playerName;
+    };
+
+    const _setNameAndTeams = (event) => {
+        const name = getPlayerNameFromUser("Who is playing?");
+        if(name === null) {
+            return;
+        }
+        const teamChosen = event.target.dataset.team
+        player1.setName(name);
+        player1.setTeam(teamChosen);
+        
+        switch(teamChosen) {
+            case "noughts":
+                computer.setTeam('crosses');
+                break;
+            case "crosses":
+                computer.setTeam('noughts');
+                break;
+            default: 
+                console.log('ERROR - invalid team chosen');
+        };
+        teamButtons.forEach(button => button.removeEventListener('click', _setNameAndTeams));
+        displayController.showPlayerNames();
+        
+        };
+
+    teamButtons.forEach(button => button.addEventListener('click', _setNameAndTeams));
+    
+    const computerMove = () => {
+
+    }; 
+
+    //
+
+    return {
+        getPlayerNameFromUser, 
+        player1,
+        computer
+    };
+})();
+
+
 
 
 //Add event listener for window
-window.addEventListener('load', Gameboard.initialiseGameBoard())
+window.addEventListener('load', Gameboard.initialiseGameBoard);
 
-window.setTimeout(displayController.renderBoard, 2000)
+displayController.renderBoard();
+window.addEventListener('keydown', () => console.table(Gameboard.getUnoccupiedSquares()));
